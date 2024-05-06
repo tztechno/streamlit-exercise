b0f6a71d74476c79e04c1e35c4f40a0f021494b6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-06 19:46:07 +0900

    Create file_uploader.py

---
+++ file_uploader.py
@@ -0,0 +1,6 @@
+    uploaded_file = st.sidebar.file_uploader("Choose a Markdown file", type=["md"])
+
+    if uploaded_file is not None:
+        markdown_text = uploaded_file.read().decode("utf-8")
+        st.markdown(f"### {uploaded_file.name}")
+        st.markdown(markdown_text)

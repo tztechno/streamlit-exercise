59a5284b53b96ed5c8cf55b753066fe8b49b6358
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-07 19:27:52 +0900

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,3 +1,4 @@
+############################################################################
 
 st.sidebar.write(f'f(x) = A * x^4 + B * x^3 + C * x^2 + D * x')
 
@@ -5,3 +6,19 @@ a = st.sidebar.slider('A', value=1, min_value=-10, max_value=10, step=1)
 b = st.sidebar.slider('B', value=1, min_value=-10, max_value=10, step=1)
 c = st.sidebar.slider('C', value=1, min_value=-10, max_value=10, step=1)
 d = st.sidebar.slider('D', value=1, min_value=-10, max_value=10, step=1)
+
+############################################################################
+
+# Sidebarの選択肢を定義する
+options = ["Option 1", "Option 2", "Option 3"]
+choice = st.sidebar.selectbox("Select an option", options)
+
+# Mainコンテンツの表示を変える
+if choice == "Option 1":
+    st.write("You selected Option 1")
+elif choice == "Option 2":
+    st.write("You selected Option 2")
+else:
+    st.write("You selected Option 3")
+
+############################################################################

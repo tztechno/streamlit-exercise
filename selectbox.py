ad2dafec1b5396010b8989e0afbff2182db96d7a
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-07 19:24:32 +0900

    Update selectbox.py

--- selectbox.py
+++ selectbox.py
@@ -1,6 +1,14 @@
+################################################
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    default=['Yellow', 'Red'] # デフォルトの設定
+)
 
+################################################
 
 option = st.selectbox(
     'Which number do you like best?', 
     ['1', '2', '3']
 )
+################################################

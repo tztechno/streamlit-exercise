f0f63d210c73b61f4e15e4335589be53f8b6454c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-07 19:22:00 +0900

    Update file_uploader.py

--- file_uploader.py
+++ file_uploader.py
@@ -1,6 +1,16 @@
-    uploaded_file = st.sidebar.file_uploader("Choose a Markdown file", type=["md"])
+####################################################################
+
+uploaded_file = st.sidebar.file_uploader("Choose a Markdown file", type=["md"])
 
     if uploaded_file is not None:
         markdown_text = uploaded_file.read().decode("utf-8")
         st.markdown(f"### {uploaded_file.name}")
         st.markdown(markdown_text)
+
+####################################################################
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.write(uploaded_file)
+
+####################################################################
